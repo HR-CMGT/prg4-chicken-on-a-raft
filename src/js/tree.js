3d3b6e403b3f20ad7d214e02b5ab56df3dbb0cdd
Erik <e.katerborg@hr.nl>
2025-05-14 15:48:56 +0200

    remove constructor arguments

--- src/js/tree.js
+++ src/js/tree.js
@@ -3,12 +3,13 @@ import { Resources } from './resources'
 import { Chicken } from './chicken'
 
 export class Tree extends Actor {
-    constructor(x, y) {
-        super({x,y,width:Resources.Tree.width, height:Resources.Tree.height})
+    constructor() {
+        super({width:Resources.Tree.width, height:Resources.Tree.height})
     }
     onInitialize(engine) {
         this.graphics.use(Resources.Tree.toSprite())
         this.vel = new Vector(Math.random()*80-120, 0)
+        this.pos = new Vector(0,0)
         this.on('exitviewport', () => this.resetPosition())
     }
 
